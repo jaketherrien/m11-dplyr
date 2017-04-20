c36f8f04ca4a494a8c38568ae8733e7e73e1e7ba
Jacques Wyatt Therrien <jake.therrien13@gmail.com>
2017-04-20 14:00:32 -0700

    finished exercise

--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -1,18 +1,24 @@
 # Exercise 6: DPLYR join introduction
 
 # Install the nycflights13 package and read it in. Require the dplyr package.
-# install.packages("nycflights13")
+install.packages("nycflights13")
 library(nycflights13)
 library(dplyr)
+View(airports)
 
 
 # Create a dataframe of the average arrival delay for each destination, then use left_join
 # to join on the "airports" dataframe, which has the airport info
-
-
+average.arrival.delay <- flights %>% group_by(dest) %>% 
+  summarise(avg.delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  mutate(faa = dest) %>% left_join(airports, by = 'faa')
+View(average.arrival.delay)
 # Create a dataframe of the average arrival delay for each airline, then use left_join
 # to join on the "airlines" dataframe, which has the airline info
-
+average.arrival.delay.airline <- flights %>% 
+  group_by(carrier) %>% 
+  summarise(avg.delay = mean(arr_delay, na.rm=TRUE)) %>% 
+  left_join(airlines, by='carrier')
 
 
 ### Bonus ###
